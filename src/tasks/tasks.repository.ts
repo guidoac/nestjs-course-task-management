decc9f6e542485b3f205062dd19dedc76cf1b4e5
Guilherme Almada <guido.ptr13@gmail.com>
2022-09-03 01:58:39 -0300

    feat(tasks/repository): added filter to search tasks

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,5 +1,3 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import { EntityRepository, Repository } from 'typeorm';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
@@ -10,9 +8,20 @@ import { Task } from './task.entity';
 export class TasksRepository extends Repository<Task> {
   async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
     const query = this.createQueryBuilder('task');
+    const { status, search } = filterDto;
+
+    if (status) {
+      query.andWhere('task.status = :status', { status });
+    }
+
+    if (search) {
+      query.andWhere(
+        'LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search)',
+        { search: `%${search}%` },
+      );
+    }
 
     const tasks = await query.getMany();
-
     return tasks;
   }
 
